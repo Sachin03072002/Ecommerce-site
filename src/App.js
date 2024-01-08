bcc13a492d7c88842b93bb1afda37032091d3a95
Sachin <sachinmondal720@gmail.com>
2024-01-08 18:26:56 +0530

    added some more pages

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import HomePage from "./customer/Pages/HomePage/HomePage";
 import Navigation from "./customer/Components/Navigation/Navigation";
 import Footer from "./customer/Components/Footer/Footer";
 import Product from "./customer/Components/Product/Product";
+import ProductDetails from "./customer/Components/ProductDetails/ProductDetails";
+import Cart from "./customer/Components/Cart/Cart";
+import Checkout from "./customer/Checkout/Checkout";
 function App() {
   return (
     <div className="App">
       <Navigation />
       <div>
         {/* <HomePage /> */}
-        <Product />
+        {/* <Product /> */}
+        {/* <ProductDetails /> */}
+        {/* <Cart /> */}
+        <Checkout />
       </div>
       <Footer />
     </div>
   );
 }
 
+
 export default App;
+
 
